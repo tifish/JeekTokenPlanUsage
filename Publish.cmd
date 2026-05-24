c0656d54df6293d8aea00c2e0fbf852626494474
Tinyfish <tifish@gmail.com>
2026-05-25 00:14:27 +0800

    Publish.cmd just publish, do not pack.

--- Publish.cmd
+++ Publish.cmd
@@ -10,14 +10,4 @@ if errorlevel 1 exit /b %errorlevel%
 
 if exist "bin\runtimes" rd /s /q "bin\runtimes"
 
-if exist "%PROJECT_NAME%.7z" del "%PROJECT_NAME%.7z"
-
-pushd bin
-7z a -r "..\%PROJECT_NAME%.7z" *
-set PACK_ERR=%errorlevel%
-popd
-if not "%PACK_ERR%"=="0" exit /b %PACK_ERR%
-
-echo.
-echo Published: %PROJECT_NAME%.7z
 endlocal
